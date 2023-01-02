5235f10ca5ba74ca68a14c2058b510e803ebd77c
Amirouche <amirouche.ibrir@gmail.com>
2023-01-02 18:59:50 +0100

    Creation class PhoneBook et Contact

--- src/main/java/com/annuaire/phonebook/PhoneBook.java
+++ src/main/java/com/annuaire/phonebook/PhoneBook.java
@@ -1,10 +1,30 @@
 package com.annuaire.phonebook;
 
+import java.util.Scanner;
+
 public class PhoneBook {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static Scanner sc = null;
 
+	public static void main(String[] args) {
+		
+		sc = new Scanner(System.in);
+		
+		String userlastName = getUserInput("Entrez un prenom: ");
+		String userFirstName = getUserInput("Entrez un nom: ");
+		String userPhoneNumber = getUserInput("Entrez un numero de tel: ");
+		
+		System.out.println(userFirstName);
+		System.out.println(userlastName);
+		System.out.println(userPhoneNumber);
+
+		sc.close();
+
+	}
+	public static String getUserInput(String userRequest) {
+		
+		System.out.println(userRequest);
+		return sc.nextLine();
 	}
 
 }
